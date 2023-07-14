88cca24239c4ad5d4c9146c0bfdb8af2e2797e85
Francis Osei Boafo <Francisrihmeek@gmail.com>
2023-07-14 14:45:10 -0500

    Feature animation added

--- src/components/Feautures-section.js
+++ src/components/Feautures-section.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { TruckIcon, ClockIcon, ShieldCheckIcon, CreditCardIcon } from '@heroicons/react/24/outline'
+import VisibilitySensor from 'react-visibility-sensor';
 
 const features = [
   {
@@ -42,16 +44,20 @@ export default function Example() {
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-            {features.map((feature) => (
-              <div key={feature.name} className="relative pl-16">
-                <dt className="text-base font-semibold leading-7 text-gray-900">
-                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                    <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+            {features.map((feature, i) => (
+              <VisibilitySensor partialVisibility key={i}>
+                {({isVisible}) =>
+                  <div key={feature.name} className={`relative pl-16 transition duration-1000 transform ${isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-8"}`}>
+                    <dt className="text-base font-semibold leading-7 text-gray-900">
+                      <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                        <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                      </div>
+                      {feature.name}
+                    </dt>
+                    <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
                   </div>
-                  {feature.name}
-                </dt>
-                <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
-              </div>
+                }
+              </VisibilitySensor>
             ))}
           </dl>
         </div>
@@ -59,4 +65,5 @@ export default function Example() {
     </div>
   )
 }
+
 
