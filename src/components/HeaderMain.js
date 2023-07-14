566ea91d9975d78c5d79dbd0b7f4e3b83163d6b1
Francis Osei Boafo <Francisrihmeek@gmail.com>
2023-07-14 02:59:00 -0500

    header fixed

--- src/components/HeaderMain.js
+++ src/components/HeaderMain.js
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { Dialog} from '@headlessui/react'
 import { Bars3Icon, XMarkIcon,ArrowRightIcon,MapPinIcon,ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline'
 import Banner from './Banners'
-import { Link } from 'react-router-dom';
 import Services from './Services-flyout'
 
 const navigation = [
@@ -47,7 +46,7 @@ export default function Example() {
         <Banner />
       </div>
       <div className="bg-white " >
-        <header className="absolute inset-x-0 top-0 z-50 pt-12">
+        <header className="absolute inset-x-0 top-1 z-50 pt-12">
           <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div className="flex lg:flex-1">
               <a href="/" className="-m-1.5 p-1.5">
