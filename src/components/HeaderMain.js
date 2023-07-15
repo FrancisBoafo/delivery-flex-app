fd3f82ffe15ffb268b321217c5c754d18584f414
Francis Osei Boafo <Francisrihmeek@gmail.com>
2023-07-15 07:58:51 -0500

    added imagebackground

--- src/components/HeaderMain.js
+++ src/components/HeaderMain.js
@@ -4,6 +4,8 @@ import { Bars3Icon, XMarkIcon,ArrowRightIcon,MapPinIcon,ChevronDownIcon, Chevron
 import Banner from './Banners'
 import Services from './Services-flyout'
 
+
+
 const navigation = [
   { 
     name: 'Home', 
@@ -41,6 +43,8 @@ export default function Example() {
 
   return (
     <>
+
+    
     
       <div className="relative z-60"> 
         <Banner />
@@ -163,64 +167,60 @@ export default function Example() {
           </Dialog>
         </header>
 
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
-        </div>
+        <div 
+        className="relative isolate px-6 pt-14 lg:px-8"
+        style={{
+          backgroundImage: 'url("https://images.unsplash.com/photo-1619096118293-a34a2f9b7339?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2050&q=80")',
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-          <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-            Become A Dasher.{' '}
-            <a href="/" className="font-semibold text-indigo-600">
-              <span className="absolute inset-0" aria-hidden="true" />
-              Read more <span aria-hidden="true">&rarr;</span>
-            </a>
-          </div>
-        </div>
-        <div className="text-center">
-          <h1 className="text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-4xl ">
-            DISCOVER DELICIOUSNESS
-          </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600 mx-auto max-w-prose">
-            Craving something delicious? Explore new and exciting dishes with Delivery Flex. 
-          </p>
-        <div className="mt-6 mx-auto max-w-prose">
-          <div className="relative">
-            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-              <MapPinIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              Become A Dasher.{' '}
+              <a href="/" className="font-semibold text-indigo-600">
+                <span className="absolute inset-0" aria-hidden="true" />
+                Read more <span aria-hidden="true">&rarr;</span>
+              </a>
             </div>
-            <input 
-              type="text" 
-              placeholder="Search for dishes..."
-              className="h-10 pl-10 pr-10 py-3 shadow-sm block w-full sm:text-sm border border-gray-300 rounded-full"
-            />
-            <button className="absolute inset-y-5 right-0 px-3 flex items-center bg-white rounded-full text-gray-400 hover:text-gray-600">
-              <ArrowRightIcon className="h-4 w-4" aria-hidden="true" />
-            </button>
+          </div>
+          <div className="text-center">
+  <h1 className="text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-4xl" style={{ fontFamily: "'Roboto', sans-serif" }}>
+    DISCOVER DELICIOUSNESS
+  </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600 mx-auto max-w-prose">
+              Craving something delicious? Explore new and exciting dishes with Delivery Flex. 
+            </p>
+            <div className="mt-6 mx-auto max-w-prose">
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <MapPinIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                </div>
+                <input 
+                  type="text" 
+                  placeholder="Search for restaurants..."
+                  className="h-10 pl-10 pr-10 py-3 shadow-sm block w-full sm:text-sm border border-gray-300 rounded-full"
+                />
+                <button className="absolute inset-y-5 right-0 px-3 flex items-center bg-white rounded-full text-gray-400 hover:text-gray-600">
+                  <ArrowRightIcon className="h-4 w-4" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="/menu"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 "
+              >
+                Order now
+              </a>
+              <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
+                Explore Menu <span aria-hidden="true">→</span>
+              </a>
+            </div>
           </div>
         </div>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="/menu"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 "
-          >
-            Order now
-          </a>
-          <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
-            Explore Menu <span aria-hidden="true">→</span>
-          </a>
-        </div>
-      </div>
-      </div>
         <div
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true"
@@ -232,9 +232,9 @@ export default function Example() {
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
           />
-        </div>
+          </div>
       </div>
-    </div>
+      </div>
     </>
   )
 }
