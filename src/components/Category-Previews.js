d4d546ef9ce49fe5868d5485902aab04aa42e196
Francis Osei Boafo <Francisrihmeek@gmail.com>
2023-07-14 19:21:59 -0500

    testing

--- src/components/Category-Previews.js
+++ src/components/Category-Previews.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const restaurants = [
   {
     name: 'Pizza Man',
-    description: 'The best Italian cuisine in town',
+    description: 'Pizza for everyone',
     imageSrc: 'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGl6emF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60',
     imageAlt: 'Restaurant 1 image',
     href: '/restaurant1',
@@ -11,7 +11,7 @@ const restaurants = [
   },
   {
     name: 'KFC',
-    description: 'The best fried chicken in town',
+    description: 'Finger lickin good',
     imageSrc: 'https://plus.unsplash.com/premium_photo-1684351369708-0eecc53e2691?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGtmY3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60',
     imageAlt: 'Restaurant 2 image',
     href: '/restaurant2',
@@ -19,7 +19,7 @@ const restaurants = [
   },
   {
     name: 'Taco Place',
-    description: 'A fantastic fusion of flavours',
+    description: 'Live más',
     imageSrc: 'https://plus.unsplash.com/premium_photo-1685316938388-c3dfe9a57ce1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGFjbyUyMGJlbGx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60',
     imageAlt: 'Restaurant 3 image',
     href: '/restaurant3',
@@ -27,7 +27,7 @@ const restaurants = [
   },
     {
     name: 'Subway',
-    description: 'Subway, eat fresh and healthy',
+    description: 'Subway, eat fresh',
     imageSrc: 'https://images.unsplash.com/photo-1521390188846-e2a3a97453a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8U3Vid2F5JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60',
     imageAlt: 'Restaurant 1 image',
     href: '/restaurant1',
@@ -35,7 +35,7 @@ const restaurants = [
   },
   {
     name: 'China Town',
-    description: 'Chinese food, try it now',
+    description: 'China made easy',
     imageSrc: 'https://plus.unsplash.com/premium_photo-1669727914916-9d093297e1da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Q2hpbmVzZSUyMGZyaWVkJTIwcmljZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60',
     imageAlt: 'Restaurant 2 image',
     href: '/restaurant2',
@@ -81,17 +81,18 @@ export default function Example() {
                         {restaurant.name}
                       </a>
                     </h3>
-                    <p className="  sm:text-sm md:text-base font-semibold text-gray-900 text-left ">{restaurant.description}</p>
+                    <p className=" sm:text-base text-sm sm:text-sm md:text-base font-semibold text-gray-900 text-left ">{restaurant.description}</p>
 
 
-                    <p className="text-base text-gray-500 text-left">{restaurant.deliveryFee}</p>
+                    <p className="base sm:text-sm text-base text-gray-500 text-left">{restaurant.deliveryFee}</p>
                   </div>
-                 <Link 
+                  <Link 
   to={restaurant.href} 
-  className="mt-4 inline-flex items-center px-3 py-2 text-sm md:px-3 md:py-2 md:text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
+  className="mt-4 inline-flex items-center px-3 py-2 text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
 >
   Order Now → 
 </Link>
+
 
               </div>
               </div>
@@ -102,6 +103,13 @@ export default function Example() {
     </div>
   )
 }
+
+
+
+  
+  
+  
+  
 
 
 
