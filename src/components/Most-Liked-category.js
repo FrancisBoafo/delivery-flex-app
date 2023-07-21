99b77425ded2abf9ed6b47b61d91956541365e82
Francis Osei Boafo <Francisrihmeek@gmail.com>
2023-07-21 17:59:08 -0500

    added now meu look

--- src/components/Most-Liked-category.js
+++ src/components/Most-Liked-category.js
@@ -1,82 +1,74 @@
+import { ShoppingCartIcon, ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/20/solid';
 import React from 'react';
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import { ShoppingCartIcon, ChevronRightIcon, ChevronLeftIcon } from '@heroicons/react/20/solid';
 
-function Arrow(props) {
-  let className = props.type === "next" ? "nextArrow" : "prevArrow";
-  className += " absolute z-10 top-1/2 -mt-3 text-black";
-  let char = props.type === "next" ? <ChevronRightIcon className="h-6 w-6"/> : <ChevronLeftIcon className="h-6 w-6"/>;
-  return (
-    <span className={className} onClick={props.onClick}>
-      {char}
-    </span>
-  );
-}
+export default function MostLikedItems({items}) {
+  const scrollRef = React.useRef(null);
 
-function MostLikedItems({ items }) {
-  // Slider settings
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    centerMode: true,
-    nextArrow: <Arrow type="next" />,
-    prevArrow: <Arrow type="prev" />,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      }
-    ]
-  };
-  
+  const scrollLeft = () => {
+    if(scrollRef.current) {
+      scrollRef.current.scrollBy({ left: -200, behavior: 'smooth' });
+    }
+  }
+
+  const scrollRight = () => {
+    if(scrollRef.current) {
+      scrollRef.current.scrollBy({ left: 200, behavior: 'smooth' });
+    }
+  }
+
   return (
-    <div className="relative">
-      <h2 className="text-xl font-semibold mb-4 text-left text-gray-900">Popular Foods</h2>
-      <Slider {...settings}>
-        {items.map((item, index) => (
-          <div key={index} className="px-4">
-            <div className="bg-white rounded-lg p-4 text-center shadow-md">
-              <div className="h-24 w-full rounded-lg overflow-hidden">
-                <img src={item.image} alt={item.name} className="h-full w-full object-cover" />
-              </div>
-              <h3 className="mt-2 text-sm font-medium text-gray-900">{item.name}</h3>
-              <p className="mt-1 text-xs text-gray-600">{item.description}</p>
-              <div className="mt-2 flex items-center justify-between">
-                <span className="text-sm font-semibold text-gray-900">${item.price}</span>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white text-xs font-bold py-1 px-2 rounded">
-                  <ShoppingCartIcon className="h-4 w-4 mr-1" />
-                  Add to Cart
-                </button>
-              </div>
-            </div>
+    <div className="bg-white">
+      <div className="py-16 sm:py-24 lg:mx-auto lg:max-w-7xl lg:px-8">
+        <div className="flex items-center justify-between px-4 sm:px-6 lg:px-0 mb-4">
+          <h2 className="text-xl font-bold text-base tracking-tight text-gray-900">Popular Dishes</h2>
+          <div className="flex space-x-2 bg-gray-200 p-2 rounded-full">
+            <ArrowLeftIcon className="h-6 w-6 text-gray-500 cursor-pointer" onClick={scrollLeft} />
+            <ArrowRightIcon className="h-6 w-6 text-gray-500 cursor-pointer" onClick={scrollRight} />
           </div>
-        ))}
-      </Slider>
+        </div>
+
+        <div className="relative">
+          <div className="relative w-full overflow-x-auto scrollbar-hide" ref={scrollRef}>
+            <ul
+              role="list"
+              className="mx-4 inline-flex space-x-8 sm:mx-6 lg:mx-0 lg:grid lg:grid-cols-4 lg:gap-x-8 lg:space-x-0"
+            >
+              {items.map((item) => (
+                <li key={item.id} className="inline-flex w-64 flex-col text-center lg:w-auto">
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200">
+                      <img
+                        src={item.imageSrc}
+                        alt={item.name}
+                        className="h-full w-full object-cover object-center group-hover:opacity-75"
+                      />
+                    </div>
+                    <div className="mt-6">
+                      <p className="text-sm text-gray-500">{item.description}</p>
+                      <h3 className="mt-1 font-semibold text-gray-900">
+                        <a href="#">
+                          <span className="absolute inset-0" />
+                          {item.name}
+                        </a>
+                      </h3>
+                      <p className="mt-1 text-gray-900">${item.price}</p>
+                      <button className="mt-2 bg-blue-500 hover:bg-blue-700 text-white text-xs font-bold py-1 px-2 rounded">
+                        <ShoppingCartIcon className="h-4 w-4 mr-1" />
+                        Add to Cart
+                      </button>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default MostLikedItems;
+
 
 
 
